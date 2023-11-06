b09fa0ca52458c85241a19a1d22f3dbcea19ffa3
MUHAMMAD IFFAT HAIKAL SAABAN HAFIZHI <iffathaikal7@gmail.com>
2023-11-07 02:40:55 +0800

    incrementdecrement counter + display items on ItemsDetailscreen

--- lib/mainScreeen/item_detail_screen.dart
+++ lib/mainScreeen/item_detail_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:number_inc_dec/number_inc_dec.dart';
 import 'package:tuition_app/widgets/app_bar.dart';
 
 import '../models/items.dart';
@@ -13,11 +14,84 @@ class ItemDetailScreen extends StatefulWidget
 }
 
 class _ItemDetailScreenState extends State<ItemDetailScreen> {
+  TextEditingController counterTextEditingController = TextEditingController();
   @override
   Widget build(BuildContext context)
   {
     return Scaffold(
       appBar: MyAppBar(),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Image.network(widget.model!.thumbnailUrl.toString()),
+          Padding(
+            padding: const EdgeInsets.all(18.0),
+            child: NumberInputPrefabbed.roundedButtons(
+              controller: counterTextEditingController,
+              incDecBgColor: Colors.cyan,
+              min: 1,
+              max: 9,
+              initialValue: 1,
+              buttonArrangement: ButtonArrangement.incRightDecLeft,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              widget.model!.title.toString(),
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20,),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              widget.model!.longDescription.toString(),
+              textAlign: TextAlign.justify,
+              style: TextStyle(fontWeight: FontWeight.normal, fontSize: 14,),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              "RM " + widget.model!.price.toString(),
+              textAlign: TextAlign.justify,
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20,),
+            ),
+          ),
+          const SizedBox(height: 10,),
+          Center(
+            child: InkWell(
+              onTap: ()
+              {
+                // add to cart
+              },
+              child: Container(
+                decoration: const BoxDecoration(
+                    gradient: LinearGradient(
+                      colors: [
+                        Colors.cyan,
+                        Colors.amber,
+                      ],
+                      begin: const FractionalOffset(0.0, 0.0),
+                      end: const FractionalOffset(1.0, 0.0),
+                      stops: [0.0,1.0],
+                      tileMode: TileMode.clamp,
+                    )
+                ),
+                width: MediaQuery.of(context).size.width - 12,
+                height: 50,
+                child: const Center(
+                  child: Text(
+                    "Book to Cart",
+                    style: TextStyle(color: Colors.white, fontSize: 16),
+                  ),
+                ),
+
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
