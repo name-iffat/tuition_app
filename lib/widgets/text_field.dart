09b4017fd760d3e4c5b0a846dc521883fd9bae5d
MUHAMMAD IFFAT HAIKAL SAABAN HAFIZHI <iffathaikal7@gmail.com>
2023-11-20 01:30:39 +0800

    fixed textfield responsive to screen

--- lib/widgets/text_field.dart
+++ lib/widgets/text_field.dart
@@ -3,14 +3,16 @@ import 'package:flutter/material.dart';
 class MyTextField extends StatelessWidget {
   final String? hint;
   final TextEditingController? controller;
+  final int? maxLine;
 
-  MyTextField({this.hint,this.controller});
+  MyTextField({this.hint,this.controller, this.maxLine});
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(8),
       child: TextFormField(
+        maxLines: null,
         controller: controller,
         decoration: InputDecoration.collapsed(hintText: hint),
         validator: (value) => value!.isEmpty ? "Field cannot be empty." : null,
