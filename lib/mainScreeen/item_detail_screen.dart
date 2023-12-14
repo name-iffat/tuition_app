c9c66c6bd0756fe967f4731561a451498e6f2c32
MUHAMMAD IFFAT HAIKAL SAABAN HAFIZHI <iffathaikal7@gmail.com>
2023-12-15 04:31:55 +0800

    allow tutor delete item

--- lib/mainScreeen/item_detail_screen.dart
+++ lib/mainScreeen/item_detail_screen.dart
@@ -1,7 +1,11 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:number_inc_dec/number_inc_dec.dart';
+import 'package:tuition_app/global/global.dart';
+import 'package:tuition_app/mainScreeen/home_screen.dart';
 import 'package:tuition_app/widgets/app_bar.dart';
+import 'package:tuition_app/widgets/simple_app_bar.dart';
 
 import '../assistantMethods/assistant_methods.dart';
 import '../models/items.dart';
@@ -18,25 +22,51 @@ class ItemDetailScreen extends StatefulWidget
 class _ItemDetailScreenState extends State<ItemDetailScreen> {
   TextEditingController counterTextEditingController = TextEditingController();
 
+  deleteItem(String itemID)
+  {
+    FirebaseFirestore.instance
+        .collection("tutors")
+        .doc(sharedPreferences!.getString("uid"))
+        .collection("subject")
+        .doc(widget.model!.subjectID!)
+        .collection("items")
+        .doc(itemID)
+        .delete().then((value)
+    {
+      FirebaseFirestore.instance
+          .collection("items")
+          .doc(itemID)
+          .delete();
+      Navigator.pushReplacement(context, MaterialPageRoute(builder: (c)=> const HomeScreen()));
+      Fluttertoast.showToast(msg: "Subject Deleted Successfully.");
+    });
+  }
 
   @override
   Widget build(BuildContext context)
   {
+    String userType = sharedPreferences!.getString("usertype")!;
+
     return Scaffold(
-      appBar: MyAppBar(tutorUID: widget.model!.tutorUID),
+      appBar: userType=="Parent" ? MyAppBar(tutorUID: widget.model!.tutorUID) : SimpleAppBar(title: sharedPreferences!.getString("name"),),
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Image.network(widget.model!.thumbnailUrl.toString()),
-          Padding(
-            padding: const EdgeInsets.all(18.0),
-            child: NumberInputPrefabbed.roundedButtons(
-              controller: counterTextEditingController,
-              incDecBgColor: Colors.cyan,
-              min: 1,
-              max: 9,
-              initialValue: 1,
-              buttonArrangement: ButtonArrangement.incRightDecLeft,
+          Center(
+            child: Image.network(widget.model!.thumbnailUrl.toString(),height: 220.0,fit: BoxFit.cover,),
+          ),
+          Visibility(
+            visible: userType == "Parent",
+            child: Padding(
+              padding: const EdgeInsets.all(18.0),
+              child: NumberInputPrefabbed.roundedButtons(
+                controller: counterTextEditingController,
+                incDecBgColor: Colors.cyan,
+                min: 1,
+                max: 9,
+                initialValue: 1,
+                buttonArrangement: ButtonArrangement.incRightDecLeft,
+              ),
             ),
           ),
           Padding(
@@ -64,7 +94,8 @@ class _ItemDetailScreenState extends State<ItemDetailScreen> {
           ),
           const SizedBox(height: 10,),
           Center(
-            child: InkWell(
+            child: userType =="Parent"
+            ? InkWell(
               onTap: ()
               {
                 int itemCounter = int.parse(counterTextEditingController.text);
@@ -98,6 +129,36 @@ class _ItemDetailScreenState extends State<ItemDetailScreen> {
                   child: Text(
                     "Book to Cart",
                     style: TextStyle(color: Colors.white, fontSize: 16),
+                  ),
+                ),
+
+              ),
+            )
+            : InkWell(
+              onTap: ()
+              {
+                //delete item
+                deleteItem(widget!.model!.itemID!);
+              },
+              child: Container(
+                decoration: const BoxDecoration(
+                    gradient: LinearGradient(
+                      colors: [
+                        Colors.cyan,
+                        Colors.blueAccent,
+                      ],
+                      begin: const FractionalOffset(0.0, 0.0),
+                      end: const FractionalOffset(1.0, 0.0),
+                      stops: [0.0,1.0],
+                      tileMode: TileMode.clamp,
+                    )
+                ),
+                width: MediaQuery.of(context).size.width - 45,
+                height: 50,
+                child: const Center(
+                  child: Text(
+                    "Delete this Subject",
+                    style: TextStyle(color: Colors.white, fontSize: 16, fontFamily: "Poppins"),
                   ),
                 ),
 
